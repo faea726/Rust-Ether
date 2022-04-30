2fda7360168299192de88c083c40adc8b0b8ed67
faea726 <fantasy.eagle96@gmail.com>
2022-04-30 21:19:47 +0700

    Create Readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# Foobar
+
+Rust-Ether is my learning curve in Rust and Blockchain.
+
+## Usage
+
+```Rust
+Cargo run
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ use std::str::FromStr;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let prvd = Provider::try_from("https://bscrpc.com")?;
+    let provider = Provider::try_from("https://bscrpc.com")?;
 
     let private_key = "039d17fedb3da5634bc09a7242c8be5d25f74eb3bdd7287ef8dc9e7e5defc0ec";
     let account = signers::Wallet::from_str(private_key)?;
 
-    let balance_acount = prvd.get_balance(account.address(), None).await?;
-    println!("{}", balance_acount);
+    let balance_acount = provider.get_balance(account.address(), None).await?;
+    println!("Blance: {}", balance_acount);
 
     Ok(())
 }
