94783aedb8ea9040b1128e4b9f944a6ac6d02735
faea726 <fantasy.eagle96@gmail.com>
2022-04-30 21:13:04 +0700

    Connect

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
-use ethers::{prelude::Provider, signers};
+use ethers::{
+    prelude::{Middleware, Provider, Signer},
+    signers,
+};
 use eyre::Result;
 use std::str::FromStr;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let provider = Provider::try_from("https://bscrpc.com")?;
+    let prvd = Provider::try_from("https://bscrpc.com")?;
 
-    let account = signers::Wallet::from_str(
-        "039d17fedb3da5634bc09a7242c8be5d25f74eb3bdd7287ef8dc9e7e5defc0ec",
-    )?;
+    let private_key = "039d17fedb3da5634bc09a7242c8be5d25f74eb3bdd7287ef8dc9e7e5defc0ec";
+    let account = signers::Wallet::from_str(private_key)?;
+
+    let balance_acount = prvd.get_balance(account.address(), None).await?;
+    println!("{}", balance_acount);
 
     Ok(())
 }
