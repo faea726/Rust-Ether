655243f49fbd2e3995c0de08faa532c9f0b4672f
faea726 <fantasy.eagle96@gmail.com>
2022-05-02 03:50:05 +0700

    create_provider

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ static PRIVATE_KEY: &str = "039d17fedb3da5634bc09a7242c8be5d25f74eb3bdd7287ef8dc
 #[tokio::main]
 async fn main() -> Result<()> {
     // Connect to Blockchain
-    let provider = Provider::try_from(NODE)?;
+    let provider = create_provider(NODE);
 
     // Create account based on private key
     let account = signers::Wallet::from_str(PRIVATE_KEY)?;
@@ -39,6 +39,7 @@ async fn main() -> Result<()> {
     let token_contract = create_contract(
         "0x8076c74c5e3f5852037f31ff0093eeb8c8add8d3",
         "./abis/ERC20-abi.json",
+        provider,
     );
 
     let token_decimals = token_contract
@@ -67,15 +68,18 @@ async fn main() -> Result<()> {
 fn create_contract(
     contract_address: &str,
     abi_path: &str,
+    contract_provider: Provider<ethers::prelude::Http>,
 ) -> Contract<Provider<ethers::prelude::Http>> {
-    let contract_provider = Provider::try_from(NODE).expect("Wrong Node");
-
     let contract_address = Address::from_str(contract_address).expect("Not Address");
 
     let file = File::open(abi_path).expect("No JSON file");
     let contract_abi: Abi = serde_json::from_reader(file).expect("Wrong JSON format");
 
     Contract::new(contract_address, contract_abi, contract_provider)
+}
+
+fn create_provider(node: &str) -> Provider<ethers::prelude::Http> {
+    Provider::try_from(node).expect("Wrong node")
 }
 
 fn from_wei(amount: U256, decimals: u8) -> f64 {
